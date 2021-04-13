1ace05a1a87fdcef304ad54681c605d40d73e503
Bradley Cantrell <cantrellbldev@gmail.com>
2021-04-12 22:08:49 -0400

    adding py script

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+color = input("Enter a color: ")
+animal = input("Enter an animal: ")
+adjective = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+
+print("My pet is a " + color + " " + animal + ". They are " + adjective + " and " + adjective2 + "!")
